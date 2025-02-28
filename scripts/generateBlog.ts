a3c6b9427a8455818cae3ddf44c076a36fab8c37
visal.naqvi <visal.naqvi@electrifai.com>
2025-02-28 23:17:44 +0530

    fixes in generateBlog script

--- scripts/generateBlog.ts
+++ scripts/generateBlog.ts
@@ -120,7 +120,8 @@ async function updateCategoryFile(existingBlogsData: Blog[], newBlog: string) {
       categoryData[categoryIndex] = currentCategory as Category
       logSuccess("Category file updated successfully")
       fs.writeFileSync(
-        process.cwd() + process.env.CATEGORY_HISTORY,
+        process.env.CATEGORY_HISTORY ||
+          "/home/visalnaqvi/datadump/GyanDost/category/data.json",
         JSON.stringify(categoryData, null, 2)
       )
       await addTogit(
@@ -298,31 +299,36 @@ export async function generateBlogTSXCode(blogRequestData: BlogRequestData) {
   logInfo(`Starting blog generation process for: "${topic}"`)
   try {
     logInfo("Generating blog inner content...")
-    logInfo(process.cwd() + process.env.BLOGS_HISTORY)
-    logInfo(process.cwd() + process.env.CATEGORY_HISTORY)
     logInfo(
-      process.cwd() +
-        `${process.env.BLOGS_DATADUMP}/${category}/${year}/${slug}/page.tsx`
+      process.env.BLOGS_HISTORY ||
+        "/home/visalnaqvi/datadump/GyanDost/blogs_history/data.json"
+    )
+    logInfo(
+      process.env.CATEGORY_HISTORY ||
+        "/home/visalnaqvi/datadump/GyanDost/category/data.json"
+    )
+    logInfo(
+      `${process.env.BLOGS_DATADUMP}/${category}/${year}/${slug}/page.tsx`
     )
     fs.writeFileSync(
-      process.cwd() + process.env.CATEGORY_HISTORY,
+      process.env.CATEGORY_HISTORY ||
+        "/home/visalnaqvi/datadump/GyanDost/category/data.json",
       JSON.stringify({hello: "world"}, null, 2)
     )
 
     fs.mkdirSync(
       path.dirname(
-        process.cwd() +
-          `${process.env.BLOGS_DATADUMP}/${category}/${year}/${slug}/page.tsx`
+        `${process.env.BLOGS_DATADUMP}/${category}/${year}/${slug}/page.tsx`
       ),
       {recursive: true}
     )
     fs.writeFileSync(
-      process.cwd() +
-        `${process.env.BLOGS_DATADUMP}/${category}/${year}/${slug}/page.tsx`,
+      `${process.env.BLOGS_DATADUMP}/${category}/${year}/${slug}/page.tsx`,
       "hello world"
     )
     fs.writeFileSync(
-      process.cwd() + process.env.BLOGS_HISTORY,
+      process.env.BLOGS_HISTORY ||
+        "/home/visalnaqvi/datadump/GyanDost/blogs_history/data.json",
       JSON.stringify({hello: "world"}, null, 2)
     )
 
@@ -367,12 +373,12 @@ export async function generateBlogTSXCode(blogRequestData: BlogRequestData) {
 
     blogsData.unshift(newBlogMeta)
     fs.writeFileSync(
-      process.cwd() +
-        `${process.env.BLOGS_DATADUMP}/${category}/${year}/${slug}/page.tsx`,
+      `${process.env.BLOGS_DATADUMP}/${category}/${year}/${slug}/page.tsx`,
       finalContent
     )
     fs.writeFileSync(
-      process.cwd() + process.env.BLOGS_HISTORY,
+      process.env.BLOGS_HISTORY ||
+        "/home/visalnaqvi/datadump/GyanDost/blogs_history/data.json",
       JSON.stringify(blogsData, null, 2)
     )
     logInfo("Updating data files...")
